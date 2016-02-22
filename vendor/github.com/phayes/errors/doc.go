2d3618eff4cb8799674ae7682b92d6a13fec792b
phayes <patrick.d.hayes@gmail.com>
2016-02-22 10:16:57 -0800

    Adding vendor files for depencies

---
+++ vendor/github.com/phayes/errors/doc.go
@@ -0,0 +1,95 @@
+/*
+Go's standard `errors` package is a simple error package for handling errors, and works fine for shallow codebases, but has several shortcomings that `phayes/errors`
+hopes to remediate.
+
+The biggest anti-pattern that the standard errors package encourages is one we are all familiar with:
+
+    if err != nil
+        return errors.New(err.Error() + ". Some more details about the error")
+    }
+
+
+This anti-pattern is corrected in phayes/errors by allowing you to cleanly wrap one error with another like so:
+
+    if err != nil {
+        return errors.Append(err, "Some more details about the error")
+    }
+
+
+This allows us to cleanly add more details to an error, while preseving the underlying error for later inspection.
+
+It also plays nicely with standard library errors and predefined errors:
+
+    import (
+        "github.com/phayes/errors"
+        "io"
+    )
+
+    var ErrFailedStream = errors.New("Failed to read stream.")
+
+    func ReadStream(b []byte) error {
+        n, err := reader.Read(b)
+        if err != nil {
+            return errors.Append(ErrFailedStream, err)
+        }
+    }
+
+    func main() {
+        var b []byte
+        err := ReadStream(b)
+        if err != nil {
+            if errors.IsA(err, io.EOF) {
+                return // Success!
+            } else {
+                log.Fatal(err) // Prints "Failed to read stream. unexpected EOF"
+            }
+        }
+    }
+
+
+Wrapping errors
+
+At it's most basic, `phayes/errors` is a drop in replacement for the standard error package.
+
+    err := errors.New("Could not parse input")
+
+However, it also provides the ability to wrap an error to give it more context:
+
+    import (
+        "github.com/phayes/errors"
+    )
+
+    func ReadStream(b []byte) error {
+        n, err := reader.Read(b)
+        if err != nil {
+	        return errors.Wraps(err, "Failed to read stream.")
+        }
+    }
+
+    func main() {
+    	var b []byte
+    	err := ReadStream(b)
+    	if err != nil {
+    		log.Fatal(err) // Prints "Failed to read stream. unexpected EOF"
+    	}
+    }
+
+
+Inspecting errors
+
+Use the `IsA` function to check to if the error, or any of it's inner errors, is what you're after. This is fully compatible with errors that
+are not part of phayes/errors. For example:
+
+    func main() {
+        var b []byte
+        err := ReadStream(b)
+        if err != nil {
+            if errors.IsA(err, io.EOF) {
+                return // Success!
+            } else {
+                log.Fatal(err)
+            }
+        }
+    }
+*/
+package errors
---
+++ vendor/github.com/phayes/errors/errors.go
@@ -0,0 +1,197 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Error interface {
+	// This returns the error message without inner errors
+	Message() string
+
+	// Get the inner error. Will return nil if there is no inner error
+	Inner() error
+
+	// Wrap the given error. Calling Inner() should retreive it. Return a copy of the outer error as an Error.
+	Wrap(error) Error
+
+	// Base() should return a copy of the Error without any inners
+	// This method is called to check two errors for equality
+	Base() error
+
+	// Implements the built-in error interface.
+	Error() string
+}
+
+// Default implementation of Error interface
+type DefaultError struct {
+	err   error
+	inner error
+}
+
+// This returns a string with error information, excluding inner errors
+func (e DefaultError) Message() string {
+	return e.err.Error()
+}
+
+// This returns a string with all available error information, including inner
+// errors that are wrapped by this errors.
+func (e DefaultError) Error() string {
+	if e.inner != nil {
+		return e.Message() + ". " + e.inner.Error()
+	} else {
+		return e.Message()
+	}
+}
+
+// Get the inner error that is wrapped by this error
+func (e DefaultError) Inner() error {
+	return e.inner
+}
+
+// Get the base error that forms the basis of the DefaultError - returns a copy of itself without inners
+func (e DefaultError) Base() error {
+	return e.err
+}
+
+// Wrap the passed error in this error and return a copy
+func (e DefaultError) Wrap(err error) Error {
+	e.inner = err
+	return e
+}
+
+// Create new error from string.
+// It intentionally mirrors the standard "errors" module so as to be a drop-in replacement
+func New(s string) error {
+	return DefaultError{
+		err: errors.New(s),
+	}
+}
+
+// Same as New, but with fmt.Printf-style parameters.
+// This is a replacement for fmt.Errorf.
+func Newf(format string, args ...interface{}) error {
+	return DefaultError{
+		err: errors.New(fmt.Sprintf(format, args...)),
+	}
+}
+
+// Append more information to the error. The reverse of Wrap.
+func Append(outerErr error, innerErr error) error {
+	if outerError, ok := outerErr.(Error); ok {
+		return outerError.Wrap(innerErr)
+	} else {
+		return DefaultError{
+			err:   outerErr,
+			inner: innerErr,
+		}
+	}
+}
+
+// Append more information to the error using a string. The reverse of Wraps.
+func Appends(outerErr error, inner string) error {
+	if outerError, ok := outerErr.(Error); ok {
+		return outerError.Wrap(New(inner))
+	} else {
+		return DefaultError{
+			err:   outerErr,
+			inner: errors.New(inner),
+		}
+	}
+}
+
+// Append more information to the error using formatting. The reverse of Wrapf.
+func Appendf(outerErr error, format string, args ...interface{}) error {
+	if outerError, ok := outerErr.(Error); ok {
+		return outerError.Wrap(Newf(format, args...))
+	} else {
+		return DefaultError{
+			err:   outerErr,
+			inner: Newf(format, args...),
+		}
+	}
+}
+
+// Wrap the first error in the second error. Reverse of Append
+func Wrap(innerErr error, outerErr error) error {
+	if outerError, ok := outerErr.(Error); ok {
+		return outerError.Wrap(innerErr)
+	} else {
+		return DefaultError{
+			err:   outerErr,
+			inner: innerErr,
+		}
+	}
+}
+
+// Wrap an error in a new error using the provided string. Reverse of Appends
+func Wraps(err error, outer string) error {
+	return DefaultError{
+		err:   errors.New(outer),
+		inner: err,
+	}
+}
+
+// Same as Wraps, but with fmt.Printf-style parameters. Reverse of Appendf
+func Wrapf(err error, format string, args ...interface{}) error {
+	return DefaultError{
+		err:   errors.New(fmt.Sprintf(format, args...)),
+		inner: err,
+	}
+}
+
+// Check to see if two errors are the same
+func Equal(e1 error, e2 error) bool {
+	if e1 == e2 {
+		return true
+	}
+
+	// Try to convert them into errors.Error and see if they are the same or if one is based on another
+	e1Error, e1ok := e1.(Error)
+	e2Error, e2ok := e2.(Error)
+
+	switch {
+	case e1ok && e2ok:
+		return e2Error.Base() == e1Error.Base()
+	case e1ok && !e2ok:
+		return e1Error.Base() == e2
+	case !e1ok && e2ok:
+		return e2Error.Base() == e1
+	case !e1ok && !e2ok:
+		return e1 == e2
+	default:
+		return false
+	}
+}
+
+// Check if two errors are the same or if the first contains the second
+// This will recursively check their inner components to see if one is an instance of the other
+func IsA(outerErr error, innerErr error) bool {
+	if Equal(outerErr, innerErr) {
+		return true
+	}
+
+	// Recursively check to see if the inner is contained in the outer
+	if outerError, ok := outerErr.(Error); ok {
+		if outerInner := outerError.Inner(); outerInner != nil {
+			return IsA(outerInner, innerErr)
+		}
+	}
+
+	// No match
+	return false
+}
+
+// Cause returns the root cause of the given error. If err does not implement phayes.Error, it returns err itself.
+func Cause(err error) error {
+	outerError, ok := err.(Error)
+	if !ok {
+		return err
+	}
+
+	if outerError.Inner() == nil {
+		return outerError.Base()
+	} else {
+		return Cause(outerError.Inner())
+	}
+}
